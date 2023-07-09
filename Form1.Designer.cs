740b88f55b45f81960ea0e8e018e2a5d029f9c65
Andrei1999CJ <cojocariuandrei32@gmail.com>
2023-07-09 18:41:24 +0300

    Added SmartWatchBatteryTracker project

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace BluetoothApp_V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bluetoothList = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bluetoothList
+            // 
+            this.bluetoothList.BackColor = System.Drawing.Color.LightGray;
+            this.bluetoothList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.bluetoothList.FormattingEnabled = true;
+            this.bluetoothList.Location = new System.Drawing.Point(86, 62);
+            this.bluetoothList.Name = "bluetoothList";
+            this.bluetoothList.Size = new System.Drawing.Size(153, 24);
+            this.bluetoothList.TabIndex = 0;
+            this.bluetoothList.SelectedIndexChanged += new System.EventHandler(this.bluetoothList_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(403, 88);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(83, 44);
+            this.progressBar.TabIndex = 2;
+            this.progressBar.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(404, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Battery Level:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(404, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bluetoothList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox bluetoothList;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Windows.Forms;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+
+namespace BluetoothApp_V1
+{
+    public partial class Form1 : Form
+    {
+        
+        DeviceInformation[] bluetoothDevices = new DeviceInformation[20];
+        private int detectedDevices = 0;
+        private bool deviceAlreadyFound;
+        private const string batteryUuid= "180f";
+        private byte receivedBatteryLevelValue;
+        private bool firstTime;
+
+        public Form1()
+        {
+            // Query for extra properties you want returned
+            string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
+
+            DeviceWatcher deviceWatcher =
+                        DeviceInformation.CreateWatcher(
+                                BluetoothLEDevice.GetDeviceSelectorFromPairingState(false),
+                                requestedProperties,
+                                DeviceInformationKind.AssociationEndpoint);
+
+            // Register event handlers before starting the watcher.
+            // Added, Updated and Removed are required to get all nearby devices
+            deviceWatcher.Added += DeviceWatcher_Added;
+            deviceWatcher.Updated += DeviceWatcher_Updated;
+            deviceWatcher.Removed += DeviceWatcher_Removed;
+
+
+            deviceWatcher.Start();
+            InitializeComponent();
+        }
+
+        private void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            // do nothing
+        }
+
+        private void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            // do nothing
+        }
+
+        private void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+            if (args.Name != "")
+            {
+                for (int j = 0; j < detectedDevices; j++)
+                {
+                    if (args.Name == bluetoothDevices[j].Name)
+                    {
+                        deviceAlreadyFound = true;
+                    }
+                }
+                if (deviceAlreadyFound == false && detectedDevices < 19) 
+                    bluetoothDevices[detectedDevices++] = args;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int j = 0; j < detectedDevices; j++)
+            {
+                if (!(bluetoothList.Items.Contains(bluetoothDevices[j].Name)))
+                {
+                    bluetoothList.Items.Add(bluetoothDevices[j].Name);
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            batteryLevelHandler(receivedBatteryLevelValue);
+        }
+
+        private void bluetoothList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = "Pairing";
+            connectToDevice(bluetoothDevices[bluetoothList.SelectedIndex]);
+        }
+
+        async void connectToDevice(DeviceInformation deviceInfo)
+        {
+            BluetoothLEDevice bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(deviceInfo.Id).AsTask();
+            GattSession gattSessionForBlDevice = await GattSession.FromDeviceIdAsync(bluetoothLeDevice.BluetoothDeviceId);
+            gattSessionForBlDevice.MaintainConnection = true;
+            GattDeviceServicesResult result = await bluetoothLeDevice.GetGattServicesAsync();
+            if (result.Status == GattCommunicationStatus.Success)
+            {
+                label1.Text = "Connected";
+                bluetoothList.Enabled = false;
+                var services = result.Services;
+                foreach (var service in services)
+                {
+                    if (service.Uuid.ToString("N").Substring(4, 4) == batteryUuid)
+                    {
+                        GattCharacteristicsResult theresult = await service.GetCharacteristicsAsync();
+                        if (theresult.Status == GattCommunicationStatus.Success)
+                        {
+                            var characteristics = theresult.Characteristics;
+                            foreach (var characteristic in characteristics)
+                            {
+                                timer2.Enabled = true;
+                                getFirstRead(characteristic);
+                                subscribeForNotifications(characteristic);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        async void subscribeForNotifications(GattCharacteristic gattCharacteristic)
+        {
+            GattCommunicationStatus status = await gattCharacteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
+                        GattClientCharacteristicConfigurationDescriptorValue.Notify);
+            if (status == GattCommunicationStatus.Success)
+            {
+                gattCharacteristic.ValueChanged += GattCharacteristic_ValueChanged;
+                batteryLevelHandler(receivedBatteryLevelValue);
+            }
+        }
+
+        async void getFirstRead(GattCharacteristic gattCharacteristic)
+        {
+            GattReadResult result = await gattCharacteristic.ReadValueAsync();
+            if (result.Status == GattCommunicationStatus.Success)
+            {
+                var reader = DataReader.FromBuffer(result.Value);
+                byte[] input = new byte[reader.UnconsumedBufferLength];
+                reader.ReadBytes(input);
+                receivedBatteryLevelValue = input[0];
+            }
+        }
+
+        private void GattCharacteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            var reader = DataReader.FromBuffer(args.CharacteristicValue);
+            byte[] input = new byte[reader.UnconsumedBufferLength];
+            reader.ReadBytes(input);
+            receivedBatteryLevelValue = input[0];
+        }
+
+        private void batteryLevelHandler(byte batteryLevel)
+        {
+            const int maximumPercentange = 100;
+            if (batteryLevel > maximumPercentange)  // This means that the smartwatch is charging
+            {
+                if (firstTime == true) // first time when the smartwatch is in charging state
+                {
+                    progressBar.Value = 0;
+                    firstTime = false;
+                }
+                label3.Text = "Charging...";
+                label3.Visible = true;
+                progressBar.Increment(5);
+                progressBar.Visible = true;
+                if (progressBar.Value == 100)
+                    progressBar.Value = 0;
+            }
+            else 
+            {
+                firstTime = true;
+                label3.Text = batteryLevel + "%";
+                label3.Visible = true;
+                progressBar.Value = batteryLevel;
+                progressBar.Visible = true;
+            }
+        }
+
+    }
+}
